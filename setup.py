a2c10515dce43aa01533d066644155db7d0d5bc2
Anton-Gladyshev <gladysha@hu-berlin.de>
2025-02-16 09:39:16 +0100

    not much

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
-setup(name="pypty", version=2.0, description="A CuPy-based set of scripts for multisclie reconstrcutions on a GPU. Can be configured for STEM (ptychograpgy), TEM (tilt or focal series) or pretty much any combination of this things.", packages=['pypty'], author="Anton Gladyshev",zip_safe=False)
+setup(name="pypty", version=2.0.0, description="A CuPy-based set of scripts for multisclie reconstrcutions on a GPU. Can be configured for STEM (ptychograpgy), TEM (tilt or focal series) or pretty much any combination of this things.", packages=['pypty'], author="Anton Gladyshev",zip_safe=False)
 
