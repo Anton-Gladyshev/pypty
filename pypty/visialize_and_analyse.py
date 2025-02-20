4347db415cc6494b48bffbc580bebc6cf9f517fc
Anton-Gladyshev <gladysha@hu-berlin.de>
2025-02-20 17:52:08 +0100

    updated plotting functions

--- pypty/visialize_and_analyse.py
+++ pypty/visialize_and_analyse.py
@@ -152,9 +152,18 @@ def add_scalebar_ax(ax, x,y, width, height, x_t, y_t, px_size, unit):
     rect.set_path_effects([PathEffects.withStroke(linewidth=2, foreground='black')])
 
 
-def outputlog_plots(path_outputlog, skip_first=0):
-    loss_path=output_folder+"loss.csv"
+def outputlog_plots(path_outputlog, skip_first=0, plot_time=True):
+    """
+    Functon for plotting log file of PyPty.
     
+    Inputs:
+        path_outputlog- pass to PyPty-csv file
+        skip_first- how many first iterations to skip (default 0)
+        plot_time- boolean Flag. If True, second x-axis showing time in seconds will be added on top of the plot.
+    Returns:
+        figs- list of plotted figures.
+    """
+    loss_path=output_folder+"loss.csv"
     dat=np.loadtxt(loss_path, skiprows=1+skip_first, delimiter=",")
     print(dat.shape)
     
@@ -164,24 +173,25 @@ def outputlog_plots(path_outputlog, skip_first=0):
     else:
         fieldnames=["epoch", "time / s", "loss", "sse", "initial step", "matching step", "N linesearch iterations",
                 "dir. derivative", "new dir. derivative", "F-axis postions reg.", "S-axis positons reg.", "S-axis tilts reg.", "F-axis tilts reg.", "l1 object reg.", "Q-space probe reg.", "R-space probe reg.", "TV object reg.", "V-object reg.", "Free GiB", "Total GiB"]
-    
     epoch=dat[:,0]
     time=dat [:, 1]
     def forward(x):
         return np.interp(x,epoch,time)
-    
     def inverse(x):
         return np.interp(x,time,epoch)
-    
+    figs=[]
     for datai in range(2, dat.shape[1], 1):
         fig,ax = plt.subplots(figsize=(10,4), dpi=300)
         ax.plot(epoch, dat[:, datai], ".-",linewidth=2, alpha=0.7)#, label=tit[iii])
         ax.set_xlabel("Iteration", fontsize = 14)
         ax.set_ylabel(fieldnames[datai], fontsize = 14)
-        ax2 = ax.secondary_xaxis("top", functions=(forward,inverse)) # Create a dummy plot
-        ax2.set_xlabel("time / s", fontsize = 14)
-        ax2.set_xscale("log")
+        if plot_time:
+            ax2 = ax.secondary_xaxis("top", functions=(forward,inverse)) # Create a dummy plot
+            ax2.set_xlabel("time / s", fontsize = 14)
+            ax2.set_xscale("log")
+        figs.append(fig)
         plt.show()
+    return figs
 
 
 
