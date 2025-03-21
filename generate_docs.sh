273b5ba12ed95bfa818f55242b063af7795dacaf
Anton-Gladyshev <gladysha@hu-berlin.de>
2025-03-21 16:42:46 +0100

    added docstings to every function, created documentation

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,4 +1,13 @@
-pip install pdoc3==0.11.6
-pip install mkdocs==1.6.1
+#!/bin/bash
+
+# Install necessary dependencies
+pip install pdoc3==0.11.6 mkdocs==1.6.1
+
+# Generate Markdown docs from Python code
 pdoc3 --output-dir docs/reference --template-dir markdown_template pypty
-mkdocs serve
+
+# Serve docs locally and open browser automatically
+mkdocs serve &
+sleep 2
+open http://127.0.0.1:8000
+wait
